8d9661a384211dad4d224ac828e3d95370342adc
padydk <pady@live.dk>
2013-05-01 19:59:33 +0200

    Tilføjet Visual Studio MVC Projekt

---
+++ VS Projects/MvcDivideStore/MvcDivideStore/Controllers/BuyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDivideStore.Controllers
+{
+    public class BuyController : Controller
+    {
+        //
+        // GET: /Buy/
+
+        public String Index(String something)
+        {
+            string message = HttpUtility.HtmlEncode("This is buy page " + something);
+
+            return message;
+        }
+
+    }
+}
---
+++ VS Projects/MvcDivideStore/MvcDivideStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDivideStore.MvcApplication" Language="C#" %>
